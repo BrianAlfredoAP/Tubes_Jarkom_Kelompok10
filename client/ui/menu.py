78c3394c2d12bf0fc2be9b116e0f9e52a4b1fc0b
Brian Alfredo AP <brianalfredodo@gmail.com>
2026-05-18 00:15:18 +0700

    Kosongkan main

--- client/ui/menu.py
+++
@@ -1,9 +0,0 @@
-def show_main_menu():
-    print("\n===== MENU =====")
-    print("1. Chat User")
-    print("2. Upload File")
-    print("3. View Received Files")
-    print("4. Help")
-    print("5. Exit")
-
-    return input("Choose: ")
